6202ce4ece5e186d1709cf09f6ef6fada57839f2
rodrigoamartins806 <104536088+rodrigoamartins806@users.noreply.github.com>
2022-04-29 14:31:15 -0300

    Primeiro Commit

---
+++ camera.py
@@ -0,0 +1,73 @@
+from time import sleep
+import cv2
+
+from tinydb import TinyDB, Query, where
+import pusher
+
+pusher_client = pusher.Pusher(
+  app_id='1400493',
+  key='502ce40f4ff4013c4bc6',
+  secret='04b8df83456e1f022d6a',
+  cluster='sa1',
+  ssl=True
+)
+
+def sendmsg(tipo, msg):
+    
+    pusher_client.trigger('my-channel', str(tipo), {'message': str(msg)})
+
+    return True
+#treinamento para detecção de face
+face_cascade=cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
+#treinamento para detecçao de corpo
+body_classifier = cv2.CascadeClassifier('haarcascade_fullbody.xml')
+
+ds_factor=0.6
+
+class VideoCamera(object):
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+        self.a =0
+        # Define the codec and create VideoWriter object
+      
+    
+    def __del__(self):
+        self.video.release()
+        
+    
+    def get_frame(self):
+        success, image = self.video.read()
+        image=cv2.resize(image,None,fx=ds_factor,fy=ds_factor,interpolation=cv2.INTER_AREA)
+        gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+        bodies = body_classifier.detectMultiScale(gray, 1.2, 3)
+        print(bodies)
+        
+        #desenha sobre o contorno do corpo
+        for (x,y,w,h) in bodies:
+            contbody =  str(bodies.shape[0])
+            cv2.rectangle(image,(x,y),(x+w,y+h),(242, 149, 71),2)
+            cv2.putText(image, "Corpo: "+contbody, (x + 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (242, 149, 71), 2, cv2.LINE_AA)
+            #print(contbody) 
+            #so envia notificação caso seja detectado
+            if int(contbody) >= 1:
+                pusher_client.trigger('my-channel', 'corpo', {'message': str(contbody)})
+            break
+
+        face_rects=face_cascade.detectMultiScale(gray,1.3,5)
+
+        #desenha sobre o rosto
+        for (x,y,w,h) in face_rects:
+            contface =  str(face_rects.shape[0]) 
+            cv2.rectangle(image,(x,y),(x+w,y+h),(164, 38, 223 ),2) 
+            cv2.putText(image, "Rosto: "+contface, (x + 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (164, 38, 223 ), 2, cv2.LINE_AA)    
+            #print(contface) 
+            #so envia notificação caso seja detectado
+            if int(contface) >= 1:
+                pusher_client.trigger('my-channel', 'rosto', {'message': str(contface)})
+            break
+        
+
+        ret, jpeg = cv2.imencode('.jpg', image)
+
+        return jpeg.tobytes()
